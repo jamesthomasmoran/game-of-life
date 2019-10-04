febb528f29557e420b7ec4674f429b2d4dd755af
James Moran <JMORA@NHSBSA.NHS.UK>
2019-10-04 08:48:18 +0100

    initial commit

---
+++ src/main/java/net/nhs/nhsbsa/App.java
@@ -0,0 +1,13 @@
+package net.nhs.nhsbsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
