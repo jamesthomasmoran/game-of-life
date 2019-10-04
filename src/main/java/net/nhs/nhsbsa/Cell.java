463b16188f19a88e39019f0e26e19fe63394cf79
jamesthomasmoran <j.moran3@newcastle.ncl.uk>
2019-10-04 09:56:22 +0100

    Generated Cell getters

--- src/main/java/net/nhs/nhsbsa/Cell.java
+++ src/main/java/net/nhs/nhsbsa/Cell.java
@@ -1,7 +1,18 @@
 package net.nhs.nhsbsa;
 
 public class Cell {
+    private int x;
+    private int y;
     public Cell(int x, int y) {
-        
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getXCoord() {
+        return x;
+    }
+
+    public int getYCoord() {
+        return y;
     }
 }
--- src/test/java/net/nhs/nhsbsa/GOLTest.java
+++ src/test/java/net/nhs/nhsbsa/GOLTest.java
@@ -18,4 +18,11 @@ public class GOLTest
         assertThat(gOL.getCellState(0,1), is(0));
     }
 
+    @Test
+    public void getCellCoords(){
+        Cell testCell = new Cell(2,5);
+        assertThat(testCell.getXCoord(), is(2));
+        assertThat(testCell.getYCoord(), is(5));
+    }
+
 }
